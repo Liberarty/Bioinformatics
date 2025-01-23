36582fcafb11426dac65f240d3caa40cda1b42fa
Liberarty <kayleigh.kostrzewa@gmail.com>
2025-01-23 18:28:37 -0500

    Added Assignment 1 from files

---
+++ learning_bash.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#Variables
+greeting="Hey Hey Hey"
+yourname=Liberty 
+
+#Echo name and learning bash
+echo "$greeting $yourname"
+echo "This script is helping you learn bash" 
+
+#make bioinformatics directory and bash script subdirectory
+mkdir -p /root/bioinformatics/bash_scripts
+
+#moving and renaming the .txt to a.sh
+mv bash_template.txt /root/bioinformatics/bash_scripts
+cd /root/bioinformatics/bash_scripts
+mv bash_template.txt bash_template.sh
